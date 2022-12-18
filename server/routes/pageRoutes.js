2d31ed7be2db56974ecb3cf9b9fb718a440396e5
CRIMSON <olueatoworosanwo@gmail.com>
2022-12-19 00:59:57 +0100

    auth stuff

--- server/routes/pageRoutes.js
+++ server/routes/pageRoutes.js
@@ -2,27 +2,14 @@ const router = require("express").Router();
 const path = require("path");
 
 router
-    .get("/", (_, res) => {
-        res.sendFile(getFile("index.html"));
-    })
-    .get("/about", (_, res) => {
-        res.sendFile(getFile("about.html"));
-    })
-    .get("/faq", (_, res) => {
-        res.sendFile(getFile("faq.html"));
-    })
-    .get("/login", (_, res) => {
-        res.sendFile(getFile("login.html"));
-    })
-    .get("/register", (_, res) => {
-        res.sendFile(getFile("register.html"));
-    })
-    .get("/pricing", (_, res) => {
-        res.sendFile(getFile("ourpricing.html"));
-    })
-    .get("/templates", (_, res) => {
-        res.sendFile(getFile("template.html"));
-    });
-module.exports = router;
+    .get("/", (_, res) => res.sendFile(getFile("index.html")))
+    .get("/about", (_, res) => res.sendFile(getFile("about.html")))
+    .get("/faq", (_, res) => res.sendFile(getFile("faq.html")))
+    .get("/login", (_, res) => res.sendFile(getFile("login.html")))
+    .get("/register", (_, res) => res.sendFile(getFile("createaccount.html")))
+    .get("/pricing", (_, res) => res.sendFile(getFile("ourpricing.html")))
+    .get("/templates", (_, res) => res.sendFile(getFile("template.html")));
 
 const getFile = (filename) => path.join(__dirname, "../../", "client", filename);
+
+module.exports = router;
